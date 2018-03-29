5a24f44e037875adcfa97446dfc734365ccab9b8
Stuart Romanek <stuart@punkave.com>
2018-03-29 11:26:50 -0400

    more resilient import for finding symbols in a deeply nested or generated svg, fix broken view rendering

--- public/js/editor-modal.js
+++ public/js/editor-modal.js
@@ -3,7 +3,7 @@ apos.define('apostrophe-svg-sprites-editor-modal', {
   transition: 'slide',
   construct: function (self, options) {
     self.afterPopulate = function (piece, callback) {
-      self.$el.find('[data-apos-svg-sprite-preview-container]').append('<svg><use xlink:href="' + piece.map + '#' + piece.id + '"></use></svg>');
+      self.$el.find('[data-apos-svg-sprite-preview-container]').append('<svg><use xlink:href="' + piece.file + '#' + piece.id + '"></use></svg>');
       return setImmediate(callback);
     }
   }
